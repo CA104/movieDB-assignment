f0eb5508384ed6e70feb068cebe79922ba3d2fc8
Carlos <carlos_arellano@hotmail.ca>
2019-05-17 15:52:10 -0400

    initiated js and JSON files

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const displayMovies = document.querySelector(`#movies`)
+const xhr = new XMLHttpRequest();
+
+displayMovies.addEventListener(`click`, function() {
+	const endPoint = `https://api.themoviedb.org/3/movie/now_playing?api_key=1af0c47628b411de73a1ff9f56cf0335&language=en-CAN&page=1`;
+	alert(`It works!`)
+	
+	xhr.open("GET", endPoint);
+	xhr.send();
+	xhr.addEventListener('readystatechange', displayPlayingNow)
+});
+
+function displayPlayingNow() {
+if (xhr.readyState == 4) {
+	const displayGallery = document.querySelector('#gallery');
+
+	const jsonData = JSON.parse(xhr.responseText);
+
+	displayGallery.innerHTML = (`${jsonData.results.poster_path}`)
+}}
