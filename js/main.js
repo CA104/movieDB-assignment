e00de8e0893051f3959385e2e618267ac7a3c014
Carlos <carlos_arellano@hotmail.ca>
2019-05-17 17:27:44 -0400

    unable to get output on DOM

--- js/main.js
+++ js/main.js
@@ -3,18 +3,18 @@ const xhr = new XMLHttpRequest();
 
 displayMovies.addEventListener(`click`, function() {
 	const endPoint = `https://api.themoviedb.org/3/movie/now_playing?api_key=1af0c47628b411de73a1ff9f56cf0335&language=en-CAN&page=1`;
-	alert(`It works!`)
+	alert(`Jon Snow!`)
 	
 	xhr.open("GET", endPoint);
 	xhr.send();
-	xhr.addEventListener('readystatechange', displayPlayingNow)
+	xhr.addEventListener('readystatechange', playingNow)
 });
 
-function displayPlayingNow() {
+function playingNow() {
 if (xhr.readyState == 4) {
 	const displayGallery = document.querySelector('#gallery');
 
 	const jsonData = JSON.parse(xhr.responseText);
 
-	displayGallery.innerHTML = (`${jsonData.results.poster_path}`)
+	displayGallery.innerHTML = (`${jsonData.results.original_title}`)
 }}
