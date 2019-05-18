626b5b656d08ff372826381762e2e69acf7cf1b3
Carlos <carlos_arellano@hotmail.ca>
2019-05-18 10:53:11 -0400

    added comments

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
-const displayMovies = document.querySelector(`#movies`)
-const xhr = new XMLHttpRequest();
+const displayMovies = document.querySelector(`#movies`) // calls the #movies <a> that will have an event listener
+const xhr = new XMLHttpRequest(); // creates HTTP request
+let listOfMovies = `{}` // empty array to add list of current movies
 
-displayMovies.addEventListener(`click`, function() {
-	const endPoint = `https://api.themoviedb.org/3/movie/now_playing?api_key=1af0c47628b411de73a1ff9f56cf0335&language=en-CAN&page=1`;
-	alert(`Jon Snow!`)
+	const endPoint = // request to the API to get currently playing list of movies
+	`https://api.themoviedb.org/3/movie/now_playing?api_key=1af0c47628b411de73a1ff9f56cf0335&language=en-CAN&page=1`; 
 	
 	xhr.open("GET", endPoint);
-	xhr.send();
-	xhr.addEventListener('readystatechange', playingNow)
-});
+	xhr.send(listOfMovies);
+	xhr.addEventListener('readystatechange', playingNow);
 
-function playingNow() {
+function playingNow() { // verifies if the request was approved and displays the content fed by the API
 if (xhr.readyState == 4) {
 	const displayGallery = document.querySelector('#gallery');
 
 	const jsonData = JSON.parse(xhr.responseText);
 
 	displayGallery.innerHTML = (`${jsonData.results.original_title}`)
-}}
+}};
