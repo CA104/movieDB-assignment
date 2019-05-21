f88a3a6fa1e51f391f68dc3174cf91fcbfae194c
Carlos <carlos_arellano@hotmail.ca>
2019-05-20 21:40:27 -0400

    added search bar, adjusted JS doc

--- js/main.js
+++ js/main.js
@@ -1,19 +1,15 @@
-const displayMovies = document.querySelector(`#movies`) // calls the #movies <a> that will have an event listener
-const xhr = new XMLHttpRequest(); // creates HTTP request
-let listOfMovies = `{}` // empty array to add list of current movies
+let data = "{}";
+const listOfMovies = document.querySelector(`#gallery`);
+const apiKey = `1af0c47628b411de73a1ff9f56cf0335`;
 
-	const endPoint = // request to the API to get currently playing list of movies
-	`https://api.themoviedb.org/3/movie/now_playing?api_key=1af0c47628b411de73a1ff9f56cf0335&language=en-CAN&page=1`; 
-	
-	xhr.open("GET", endPoint);
-	xhr.send(listOfMovies);
-	xhr.addEventListener('readystatechange', playingNow);
+let xhr = new XMLHttpRequest();
+xhr.addEventListener("readystatechange", function () {
+  if (xhr.readyState == 4) {
+	  const jsonData = JSON.parse(xhr.responseText);
+    listOfMovies.innerHTML = (`${jsonData.results.overview}`)
+  }
+});
 
-function playingNow() { // verifies if the request was approved and displays the content fed by the API
-if (xhr.readyState == 4) {
-	const displayGallery = document.querySelector('#gallery');
+xhr.open("GET", `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-CAN`);
 
-	const jsonData = JSON.parse(xhr.responseText);
-
-	displayGallery.innerHTML = (`${jsonData.results.original_title}`)
-}};
+xhr.send(data);
