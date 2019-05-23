eb586ffc8a17560254ee02125e27014e44bd3961
Carlos <carlos_arellano@hotmail.ca>
2019-05-23 17:44:40 -0400

    created new request

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
-let data = "{}";
-const listOfMovies = document.querySelector(`#gallery`);
+const displayMovies = document.querySelector(`#movies`) // calls the #movies <a> that will have an event listener
+const displayGallery = document.querySelector('#gallery');
 const apiKey = `1af0c47628b411de73a1ff9f56cf0335`;
+const xhr = new XMLHttpRequest(); // creates HTTP request
 
-let xhr = new XMLHttpRequest();
-xhr.addEventListener("readystatechange", function () {
-  if (xhr.readyState == 4) {
-	  const jsonData = JSON.parse(xhr.responseText);
-    listOfMovies.innerHTML = (`${jsonData.results[0].overview}`)
-  }
-});
+window.addEventListener(`load`, function(){
+    const endPoint = // request to the API to get currently playing list of movies
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-CAN`; 
+    console.log(`${endPoint}`)
+    
+    xhr.open("GET", endPoint);
+    xhr.send(listOfMovies);
+    xhr.addEventListener('readystatechange', playingNow);
+})
 
-xhr.open("GET", `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-CAN`);
-
-xhr.send(data);
+function playingNow() { // verifies if the request was approved and displays the content fed by the API
+if (xhr.readyState == 4) {
+    const jsonData = JSON.parse(xhr.responseText);
+    console.log(`${jsonData.results.overview}`)
+}};
