122f262bcef9e9a347e2d2713a5fe4cc573567e4
Carlos <carlos_arellano@hotmail.ca>
2019-05-26 17:05:34 -0400

    created buttons and added styling

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const displayMovies = document.querySelector(`#movies`)  // calls the #movies <a
 const apiKey = `1af0c47628b411de73a1ff9f56cf0335`;
 const xhr = new XMLHttpRequest();  // creates HTTP request
 
-window.addEventListener(`load`, function(){
+window.addEventListener(`load`, e => {
     const endPoint =  // request to the API to get currently playing list of movies
     `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`; 
     console.log(`${endPoint}`)
@@ -19,13 +19,12 @@ function playingNow() {   // verifies if the request was approved and displays t
 
   let i;
   for( i = 0; i < jsonData.results.length; i++) {
-    console.log(`${jsonData.results[i].original_title} - ${jsonData.results[i].overview}`);  // prints array of movies on the console 
 
   displayGallery.innerHTML +=  // prints the array objects on the DOM
   `<div class= "movie"><h2>${jsonData.results[i].original_title}</h2> 
   <p>Rating: ${jsonData.results[i].vote_average}</p> 
   <p>${jsonData.results[i].release_date}</p> 
   <div><p>${jsonData.results[i].overview}</p></div> 
-  <img class= "poster" src= "http://image.tmdb.org/t/p/w200/${jsonData.results[i].poster_path}"></div>`; 
+  <img id="moreInfo" class= "poster" src= "http://image.tmdb.org/t/p/w200/${jsonData.results[i].poster_path}"></div>`; 
   }
 }};
