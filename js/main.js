48c8e74d32ee720d6dfd8aaacacc9345d7db607c
Carlos <carlos_arellano@hotmail.ca>
2019-05-27 21:25:55 -0400

    pagination fully functional

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
-const displayMovies = document.querySelector(`#movies`)  // calls the #movies <a> that will have an event listener
+/***** calls the #movies <a> that will have an event listener *****/
+const displayMovies = document.querySelector(`#movies`)  
 const apiKey = `1af0c47628b411de73a1ff9f56cf0335`;
 const xhr = new XMLHttpRequest();  // creates HTTP request
 
+/***** sends request to the API for content *****/
 const createRequest = (page=1) => {
-window.addEventListener(`load`, e => {
-    const endPoint =  // request to the API to get currently playing list of movies
+    const endPoint = 
     `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=${page}`; 
     console.log(`${endPoint}`)
     
     xhr.open("GET", endPoint);
     xhr.send();
     xhr.addEventListener('readystatechange', playingNow);
-})
 }
-createRequest();
 
-function playingNow() {   // verifies if the request was approved and displays the content fed by the API
+/***** verifies if the request was approved and displays the content fed by the API *****/
+function playingNow() {   
   if (xhr.readyState == 4) {
   const jsonData = JSON.parse(xhr.responseText);
   const displayGallery = document.querySelector('#galleryOfMovies');
 
+/***** loops through the array and prints the movie titles, overview, rating, release date and poster  *****/
   let i;
   for( i = 0; i < jsonData.results.length; i++) {
-
   displayGallery.innerHTML +=  // prints the array objects on the DOM
   `<div class= "movie"><h2>${jsonData.results[i].original_title}</h2> 
   <p>Rating: ${jsonData.results[i].vote_average}</p> 
@@ -32,6 +32,10 @@ function playingNow() {   // verifies if the request was approved and displays t
   }
 }};
 
+/***** prints the list of now playing movies *****/
+createRequest();
+
+/***** resend the reuest to the API for the page number for the event fired *****/
 const changePage = document.getElementById(`pagination`);
 changePage.addEventListener(`click`, event => {
   if( !event.target.matches(`li`) ) {
