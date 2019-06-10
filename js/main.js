123bc9dd21caad535f2350926392cb53a5b33a69
Carlos <carlos_arellano@hotmail.ca>
2019-06-10 14:58:51 -0400

    call for id working

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const apiKey = `1af0c47628b411de73a1ff9f56cf0335`;
 const xhr = new XMLHttpRequest();  // creates HTTP request
 const displayGalleryAsHTML = document.querySelector('#galleryOfMovies');
 const movies = document.querySelector(`#movies`);
+const singleMovieView = document.querySelector(`#singleMovie`);
 
 /*****  sends request to the API for content  *****/
 const createRequestForMovies = (page=1) => {
@@ -23,12 +24,15 @@ const playingNow = () => {
 
 /*****  loops through the array and prints the movie titles, overview, rating, release date and poster   *****/
   for( let i = 0; i < jsonData.results.length; i++) {
-    displayGalleryAsHTML.innerHTML +=  // prints the array objects on the DOM
-  `<div class= "movie"><h2 class= "animated fadeIn" data-movie=${jsonData.results[i].id}>${jsonData.results[i].original_title}</h2> 
+    displayGalleryAsHTML.innerHTML +=
+  `<div class="movie" data-id="${jsonData.results[i].id}">
+  <h2 class= "animated fadeIn">${jsonData.results[i].original_title}</h2> 
   <p class= "animated fadeIn">Rating: ${jsonData.results[i].vote_average}</p> 
   <p class= "animated fadeIn">${jsonData.results[i].release_date}</p> 
   <p class= "overview animated fadeIn">${jsonData.results[i].overview}</p>
-  <img class= "poster animated fadeIn" src= "http://image.tmdb.org/t/p/w200/${jsonData.results[i].poster_path}"></div>`; 
+  <img data-movie=${jsonData.results[i].id} id="${jsonData.results[i].id}" class= "poster animated fadeIn" src= "http://image.tmdb.org/t/p/w200/${jsonData.results[i].poster_path}">
+  </div>
+  `; 
   }
 }};
 
@@ -42,22 +46,34 @@ changePage.addEventListener(`click`, event => {
   }
   createRequestForMovies( event.target.dataset.page);
   console.log(`event.target.page`)
+  window.scrollTo(0, 0);
 })
 
 /*****  gets the ele ID when poster is clicked  *****/
 displayGalleryAsHTML.addEventListener(`click`, event => {
-  if (event.target.matches(`img`)) {
-    console.log(playingNow(event.target.dataset.movie))
-  }
-})
-
-
-const requestForSingleMovie = id => {
-  const singleMovieEndPoint = `https://api.themoviedb.org/3/movie/${id}?api_key=1af0c47628b411de73a1ff9f56cf0335&language=en-US`;
+  let getMovieId = event.target.closest(`.movie`);
+  const xhrForSingleMovie = new XMLHttpRequest();
+  
+  const singleMovieEndPoint = `https://api.themoviedb.org/3/movie/${getMovieId.dataset.id}?api_key=${apiKey}&language=en-US`;
   console.log(`${singleMovieEndPoint}`);
 
-  xhr.open(GET, singleMovieEndPoint);
-  xhr.send();
-  xhr.addEventListener(`readystatechange`, singleMovie);
-}
+  xhrForSingleMovie.open(`GET`, singleMovieEndPoint);
+  xhrForSingleMovie.send();
+  xhrForSingleMovie.addEventListener(`readystatechange`, singleMovie);
+});
+
+const singleMovie = () => {
+  if (this.readyState == 4) {
+    const jsonDataForMovie = JSON.parse(xhrForSingleMovie.responseText);
+    displayGalleryAsHTML.innerHTML = ``;
+
+    for( let i = 0; i < jsonData.results.length; i++) {
+      singleMovieView.innerHTML = `
+      <div class= "movie"><h2 class= "animated fadeIn" data-movie=${jsonDataForMovie.results[i].id}>${jsonDataForMovie.results[i].original_title}</h2> 
+      <p class= "animated fadeIn">${jsonDataForMovie.results[i].release_date}</p> 
+      <p class= "overview animated fadeIn">${jsonDataForMovie.results[i].overview}</p>
+      <img class= "poster animated fadeIn" src= "http://image.tmdb.org/t/p/w200/${jsonDataForMovie.results[i].poster_path}"></div>
+      `
+    }
+}};
 
