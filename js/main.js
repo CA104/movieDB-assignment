4d889be7875126f0f95b4bb76b76d5a63a52af65
Carlos <carlos_arellano@hotmail.ca>
2019-06-12 23:45:23 -0400

    styling for single view

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const xhr = new XMLHttpRequest();  // creates HTTP request
 const displayGalleryAsHTML = document.querySelector('#galleryOfMovies');
 const movies = document.querySelector(`#movies`);
 const singleMovieView = document.querySelector(`#singleMovie`);
+const movieTrailer = document.querySelector(`#movieTrailer`);
 
 /*****  sends request to the API for content  *****/
 const createRequestForMovies = (page=1) => {
@@ -46,7 +47,7 @@ changePage.addEventListener(`click`, event => {
   }
   createRequestForMovies( event.target.dataset.page);
   console.log(`event.target.page`)
-  window.scrollTo(0, 0);
+  window.scrollTo(0,0);
 })
 
 /*****  gets the ele ID when poster is clicked and hides the list of movies from the DOM *****/
@@ -67,12 +68,14 @@ displayGalleryAsHTML.addEventListener(`click`, event => {
       const jsonDataForMovie = JSON.parse(this.responseText);
 
       singleMovieView.innerHTML = 
-      `<div class= "single-movie">
+      `<div class= "single-movie animated fadeIn">
       <h2 data-movie=${jsonDataForMovie.id}>${jsonDataForMovie.original_title}</h2> 
       <p>${jsonDataForMovie.release_date}</p> 
       <p>${jsonDataForMovie.overview}</p>
       <img src= "http://image.tmdb.org/t/p/w500/${jsonDataForMovie.poster_path}"></div>
       `
+      changePage.innerHTML = ``;
+      window.scrollTo(0,260);
     }}}
 });
 
@@ -81,3 +84,26 @@ movies.addEventListener(`click`, event => {
   createRequestForMovies();
   window.location.reload();
 })
+
+/*****  listener to request API videos request *****/
+displayGalleryAsHTML.addEventListener(`click`, event => {
+  if(event.target.matches(`img`)) {
+
+    let getTrailer = event.target.closest(`.movie`);
+    const xhrForTrailer = new XMLHttpRequest();
+    
+    const trailerEndPoint = `https://api.themoviedb.org/3/movie/${getTrailer.dataset.id}/videos?api_key=${apiKey}&language=en-US`;
+    console.log(trailerEndPoint);
+    displayGalleryAsHTML.innerHTML = ``;
+  
+    xhrForTrailer.open(`GET`, trailerEndPoint);
+    xhrForTrailer.send();
+    xhrForTrailer.onreadystatechange = function() {
+      if (this.readyState == 4) {
+        const jsonDataTrailer = JSON.parse(this.responseText);
+
+        movieTrailer.innerHTML = `
+        <a href="https://www.youtube.com/watch?v=${jsonDataTrailer[0].key}"></a>
+        `
+      }}}
+});
